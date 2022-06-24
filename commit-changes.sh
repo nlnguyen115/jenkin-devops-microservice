e266240ca06b88f43b8a5b16715baa3b0bfcd25d
Le Nguyen <lenguyen@Nguyens-MacBook-Air.local>
2022-06-24 16:22:24 +0800

    adding all changes

--- commit-changes.sh
+++ commit-changes.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 #commit all changes to github
+
+echo ">>>> git add *"
 git add *
-git commit -m "adding another stage"
+
+echo ">>>> git commit -m adding all changes"
+git commit -m "adding all changes"
+
+echo ">>>> git push -u origin main"
 git push -u origin main
